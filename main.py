dad3fd77bfb79293f872e1252df831a03a158699
Nelson de Barros <nelsinho.527@gmail.com>
2022-12-12 12:03:15 -0300

    delete csv

--- main.py
+++ main.py
@@ -15,40 +15,31 @@ def parse():
     for item in items:
         name_list.append(item.find('h2', class_='a-size-base').text.lstrip())
         price_list.append(item.find('span', class_='a-offscreen').text.lstrip())
-        #print(f"{name} {price}")
     for n, p  in zip(name_list, price_list):
         print(n, p)
     return name_list, price_list
 
-def convertToJSON(data1, data2):
-
+def convertToJSON(*args):
 
     list = []
-    for i in range(0,len(data1)):
-        list.append({"item" : data1[i], "price" : data2[i]})
+    for i in range(0,len(args)):
+        list.append({"item" : args[0][i], "price" : args[1][i]})
     with open('wishlist.json', 'w') as file:
         file.write(json.dumps(list, indent=1))
         return 0
 
-#def convertTOCSV(data1, data2):
-
-    
 def main():
     data = parse()
-    print("Convert data? y/n")
+    print("Export data? y/n")
     answer = input("")
     if answer == 'n':
         return 0
     else:
-        print("1 - JSON \n2 - CSV")
+        print("1 - JSON")
         answer = input("")
         if answer == '1':
             convertToJSON(data[0], data[1])
             return 0
-        # elif answer == '2':
-        #     convertTOCSV(data[0], data[1])
-        #     return 0
-
 
 if __name__ == "__main__":
     main()
